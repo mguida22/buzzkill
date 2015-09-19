8a8ffcc51714564093ad8dd1fc312b38e2ef1898
mguida22 <michael.guida@colorado.edu>
2015-09-19 17:07:15 -0600

    matching common title phrases

---
+++ dict.js
@@ -0,0 +1,6 @@
+var dict = {
+  "phrases": ["and this is what happen", "everything you need to know", "how well do you know", "this is what happen", "you wont believe what happen", "guess what happen", "wants you to find out", "totally unexpected", "reasons why", "watch this guy", "watch this girl", "hear one man", "hear one woman", "until one man", "until one woman", "the result is", "reasons to love", "reasons to hate", "find out what", "woah!", "awesome!", "check out these", "look at this", "look at these"],
+  "number_phrases": ["things", "times", "ways", "weird", "pictures", "people", "songs", "signs", "reasons", "celebrity", "vintage"]
+};
+
+var newTitle = "Clickbait Title.";
--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 $(function() {
   var title;
+
   $("a").each(function() {
     title = $(this).text();
+    title = title.trim().toLowerCase();
 
-    if (title && title.match(/\d+/g) !== null) {
-      console.log(title);
+    var _this = this;
+
+    if (title) {
+      dict["phrases"].forEach(function(phrase) {
+        if (title.indexOf(phrase) > -1) {
+          console.log(title);
+          $(_this).text(newTitle);
+        }
+      });
+    }
+
+    if (title && title.match(/\d+/g)) {
+      dict["number_phrases"].forEach(function(phrase) {
+        if (title.indexOf(phrase) > -1) {
+          console.log(title);
+          $(_this).text(newTitle);
+        }
+      });
     }
   });
+
 });
