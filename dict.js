38d3556ea94255c33951029995b1bbfa4a97a083
mguida22 <michael.guida@colorado.edu>
2015-09-22 13:21:32 -0600

    restructure and add support for scrolling

--- dict.js
+++ dict.js
@@ -1,6 +1,6 @@
 var dict = {
   "phrases": ["and this is what happen", "everything you need to know", "how well do you know", "this is what happen", "you won't believe what happen", "guess what happen", "wants you to find out", "totally unexpected", "reasons why", "watch this guy", "watch this girl", "hear one man", "hear one woman", "until one man", "until one woman", "the result is", "reasons to love", "reasons to hate", "find out what", "woah!", "awesome!", "check out these", "look at this", "look at these", "can you guess", "who is your", "what is your", "are you", "can we guess", "how well do you", "garunteed to make you", "insane ways", "zodiac", "should you get", "you can't miss", "don't miss", "watch this if"],
-  "number_phrases": ["things", "times", "ways", "weird", "pictures", "people", "songs", "signs", "reasons", "celebrity", "vintage", "websites", "questions", "thoughts", "ideas", "secrets", "tricks", "books"],
+  "number_phrases": ["things", "times", "ways", "weird", "pictures", "people", "songs", "signs", "reasons", "celebrity", "vintage", "websites", "questions", "thoughts", "ideas", "secrets", "tricks", "books", "confessions"],
   "blocked_urls": ["bzfd.it", "buzzfeed.com", "clickhole.com", "u.pw", "upworthy.com"]
 };
 
