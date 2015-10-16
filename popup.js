89701a1e7c8ee436777d22acddc793c14f21cdf6
Andrew Gentry <agentdevr@gmail.com>
2015-10-15 18:24:20 -0600

    Latest style update. Added circle activate and sensitivity buttons

--- popup.js
+++ popup.js
@@ -10,4 +10,41 @@ function getCurrentTabUrl(callback) {
     var url = tab.url;
     callback(url);
   });
+
 }
+
+  
+$("#slider").roundSlider({
+    value: 50,
+    radius: 43,
+    width: 6,
+    handleSize: "10,8",
+    handleShape: "square",
+    sliderType: "min-range",
+    mouseScrollAction: true,
+    editableTooltip: false,
+    tooltipFormat: "changeTooltip"
+
+
+});
+
+
+    function changeTooltip(e) {
+    var val = e.value, sensitivity;
+    if (val < 20) sensitivity = "Low";
+    else if (val < 40) sensitivity = "Normal";
+    else if (val < 70) sensitivity = "High";
+    else sensitivity = "Everything";
+
+    return val;
+}
+
+$('#demo').famultibutton({
+	classes: ['fa-4x'],
+	icon: 'fa-bolt',
+    onColor: '#000',
+    offColor: '#ffffff',
+    onBackgroundColor: '#3498DB',
+    offBackgroundColor: '#bbb',
+    mode: 'toggle', 
+});
